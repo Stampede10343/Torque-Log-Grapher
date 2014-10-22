14221e70f110112ccd1dce6e946ebd0647ec2f48
Stampede10343 <stampede10343@gmail.com>
2014-10-22 16:59:08 -0400

    Added another graph. Working on making it look pretty

--- TorqueGraph.py
+++ TorqueGraph.py
@@ -3,76 +3,92 @@ import matplotlib.pyplot as plt
 #f = raw_input("Enter file name: ")
 inputFile = open("torqueTrackLog1.txt")
 
-header = inputFile.readline() #first line
+header = inputFile.readline()  # first line
 
 print(header)
-        
-    
+
+
 class LogEntry:
 
-    def __init__(self, Time):
-        self.time = Time
-    time = ""    
-    Gx = 0.0
-    Gy = 0.0
-    Gz = 0.0
-    engineLoad = 0.0
-    coolantTemp = 0
-    intakePressure = 0
-    RPM = 0
-    timingAdv = 0
-    intakeTemp = 0
-    fuelTrim = 0.0
+	def __init__(self, time):
+		self.time = time
+	time = ""
+	Gx = 0.0
+	Gy = 0.0
+	Gz = 0.0
+	engineLoad = 0.0
+	coolantTemp = 0
+	intakePressure = 0
+	RPM = 0
+	timingAdv = 0
+	intakeTemp = 0
+	fuelTrim = 0.0
 
 
-def getValues(file, logEntry):
-    line = file.readline()
+def getValues(file):
+	vLine = file.readline()
 
-    valueList = line.split(',')
+	valueList = vLine.split(',')
 
-    #print(valueList)
-    entry = LogEntry(valueList[1])
+	#print(valueList)
+	entry = LogEntry(valueList[1])
 
-    entry.Gx = valueList[8]
-    entry.Gy = valueList[9]
-    entry.Gz = valueList[10]
-    entry.engineLoad = valueList[12]
-    entry.coolantTemp = valueList[13]
-    entry.intakePressure = valueList[14]
-    entry.RPM = valueList[15]
-    entry.timingAdv = valueList[17]
-    entry.intakeTemp = valueList[18]
-    entry.fuelTrim = valueList[19]
+	entry.Gx = valueList[8]
+	entry.Gy = valueList[9]
+	entry.Gz = valueList[10]
+	entry.engineLoad = valueList[12]
+	entry.coolantTemp = valueList[13]
+	entry.intakePressure = valueList[14]
+	entry.RPM = valueList[15]
+	entry.timingAdv = valueList[17]
+	entry.intakeTemp = valueList[18]
+	entry.fuelTrim = valueList[19]
 
-    return entry
+	return entry
 
 def stripTime(time):
 	stripped = time[(time.find(':')-2):-4]
 	
 	stripped = stripped.replace(":","")
-    
+
 	return stripped
 
 dataList = []
 
-for entry in inputFile.readline():
-    dataLine = getValues(inputFile, entry)
-    dataList.append(dataLine)
-    
+for line in inputFile.readline():
+	dataLine = getValues(inputFile)
+	dataList.append(dataLine)
+
 
 print(stripTime(dataList[1].time))
 
-x = []
-y = []
+timeList = []
+rpmList = []
+timing = []
+engLoad = []
 for data in dataList:
-	#plt.plot([stripTime(data.time)],[data.RPM])
-        x.append(stripTime(data.time))
-        y.append(data.RPM)
+	timeList.append(stripTime(data.time))
+	rpmList.append(data.RPM)
+	t = float(data.timingAdv)*20
+	timing.append(t)
+	e = float(data.engineLoad)*10
+	engLoad.append(e)
 
-plt.plot(x,y)
+fig = plt.figure()
+fig.add_subplot(211)
+
+rpmPlot = fig.add_subplot(212)
+loadPlot = fig.add_subplot(211)
+
+
+rpmPlot.plot(timeList,rpmList)
+loadPlot.plot(timeList,timing)
+plt.plot(timeList, engLoad)
+
 plt.xlim(int(stripTime(dataList[0].time)),int(stripTime(dataList[(len(dataList)-1)].time)))
 plt.ylabel("RPM")
 plt.ylim(0,5500)
 
-#plt.draw()
+plt.title("RPM and Timing Advance")
+
 plt.show()
